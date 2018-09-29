ab9f3e2ed4c610d55127313ad882ae0a83718393
Berenique <enjoythemilk@gmail.com>
2018-09-30 00:33:27 +0300

    Сделали хуй

---
+++ SexyFoxy/Assets/Sources/HuyEgoZnayet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuyEgoZnayet : MonoBehaviour {
+
+    public int Vidimaya;
+    private int Nevidimaya;
+	// Use this for initialization
+	void Start () {
+        HuyEgoZnayet HZ;
+        HZ.Vidimaya = 1;
+        HZ.Nevidimaya = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SexyFoxy/Assets/Sources/HuyMagic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class HuyMagic : MonoBehaviour {
+
+    public GameObject SpawnPrefab;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        for (int i = 0; i < 3; i++)
+        {
+            var miniHuy = Instantiate(SpawnPrefab, transform.position, Random.rotation);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
